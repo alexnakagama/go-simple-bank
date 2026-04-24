a57099b1013a41b29d5a5f16f229fbd6933ff36a
alex <alexnakagama4@gmail.com>
2026-04-24 15:07:39 -0300

    added more test and edited the query so it returns both account and err

--- internal/db/query/accounts.sql
+++ internal/db/query/accounts.sql
@@ -17,10 +17,11 @@ ORDER BY id
 LIMIT $1
 OFFSET $2;
 
--- name: UpdateAccount :exec
+-- name: UpdateAccount :one
 UPDATE accounts
 SET balance = $2
-WHERE id = $1;
+WHERE id = $1
+RETURNING id, owner, balance, currency, created_at;
 
 -- name: DeleteAccount :exec
 DELETE FROM accounts
