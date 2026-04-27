2e37b8481ee759ae087a48b9ead0b9a77af47d89
alex <alexnakagama4@gmail.com>
2026-04-26 23:46:35 -0300

    created the server and api endpoints

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
+	"log"
+	"os"
+	"github.com/alexnakagama/go-simple-bank/internal/api"
+	db "github.com/alexnakagama/go-simple-bank/internal/db/sqlc"
+	_ "github.com/lib/pq"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello world!")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("cannot load .env file:", err)
+	}
+	dbSource := os.Getenv("DB_SOURCE")
+
+	sqlDB, err := sql.Open("postgres", dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	store := db.NewStore(sqlDB)
+	server := api.NewServer(store)
+
+	if err := server.Start(":8080"); err != nil {
+		log.Fatal("cannot start server:", err)
+	}
 }
---
+++ internal/api/account.go
@@ -0,0 +1,62 @@
+package api
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+	"database/sql"
+	db "github.com/alexnakagama/go-simple-bank/internal/db/sqlc"
+)
+
+type createAccountRequest struct {
+	Owner string `json:"owner" binding:"required"`
+	Currency string `json:"currency" binding:"required,oneof=USD EUR"`
+}
+
+func (s *Server) createAccount(ctx *gin.Context) {
+	var req createAccountRequest
+	err := ctx.ShouldBindJSON(&req)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.CreateAccountParams{
+		Owner: req.Owner,
+		Currency: req.Currency,
+		Balance: 0,
+	}
+
+	account, err := s.store.CreateAccount(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
+
+type getAccountRequest struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+func (s *Server) getAccount(ctx *gin.Context) {
+	var req getAccountRequest
+	err := ctx.ShouldBindUri(&req)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	account, err := s.store.GetAccount(ctx, req.ID)
+	if err != nil {
+
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
