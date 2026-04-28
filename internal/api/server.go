f697ede89da95083de5500b34c1258349b83a7cf
alex <alexnakagama4@gmail.com>
2026-04-28 17:10:28 -0300

    implemented validator for the params when transfering money

--- internal/api/server.go
+++ internal/api/server.go
@@ -3,6 +3,8 @@ package api
 import (
 	db "github.com/alexnakagama/go-simple-bank/internal/db/sqlc"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 )
 
 type Server struct {
@@ -15,6 +17,11 @@ func NewServer(store db.Store) *Server {
 		store: store,
 	}
 	router := gin.Default()
+
+	v, ok := binding.Validator.Engine().(*validator.Validate)
+	if ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
 
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
